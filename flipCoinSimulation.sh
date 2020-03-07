861e60a67a196c0ec7f932e257d334d3f6b64d93
kirtibhave <kirtibhave1000@gmail.com>
2020-03-07 11:40:42 +0530

    ADDED:Create dictionary to calculate percentage of singlet combination

--- flipCoinSimulation.sh
+++ flipCoinSimulation.sh
@@ -1,12 +1,20 @@
-#!/bin/bash -x
+#!/bin/bash 
 echo "**********Welcome to Flip Coin Simulation problem**********"
 
+declare -A singleCoinDictionary
 HEAD=0
 TAIL=1
-flipCoin=$((RANDOM%2))
-if [ $HEAD == $flipCoin ]
-then
-	echo "HEAD"
-else
-	echo "TAIL"
-fi
+for(( i=1;i<=15;i++ ))
+do
+  flipCoin=$((RANDOM%2))
+  if [ $flipCoin -eq 1 ]
+  then
+       singleCoinDictionary[HEAD]=$((${singleCoinDictionary[HEAD]}+1))
+       HEAD_PER=$((${singleCoinDictionary[HEAD]}*100/15))
+  else
+       singleCoinDictionary[TAIL]=$((${singleCoinDictionary[TAIL]}+1))
+       TAIL_PER=$((${singleCoinDictionary[TAIL]}*100/15))
+  fi
+done
+echo ${!singleCoinDictionary[@]}
+echo "percentage is:" ${singleCoinDictionary[@]}
